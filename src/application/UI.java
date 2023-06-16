e2b1d7b94d91b9927fb28d6e82de64f19b5a0c5c
lacerdajcarlos <lacerda.jcarlos@gmail.com>
2023-06-16 18:23:14 +0100

    Implementing possible moves of King

--- src/application/UI.java
+++ src/application/UI.java
@@ -43,16 +43,16 @@ public class UI {
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		} catch (RuntimeException e) {
+		} 
+		catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. valid values are from a1 to h8.");
 		}
 	}
-
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 
 			}
 			System.out.println();
@@ -60,10 +60,29 @@ public class UI {
 		System.out.print("  a b c d e f g h");
 
 	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
 
-	private static void printPiece(ChessPiece piece) {
+			}
+			System.out.println();
+		}
+
+		System.out.print("  a b c d e f g h");
+	}
+
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+
+		}
 		if (piece == null) {
-			System.out.print("_");
+			System.out.print("_" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
@@ -73,5 +92,8 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
+
+
+
 
 }
